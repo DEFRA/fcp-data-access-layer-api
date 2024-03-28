d82a339157112259af9e85c463898a0ea1b14d04
Hadi <hadi.tajallaei@defra.gov.uk>
2024-03-28 10:47:15 +0000

    added a condition to retun null when sbi is not available

--- app/graphql/resolvers/customer/customer.js
+++ app/graphql/resolvers/customer/customer.js
@@ -14,11 +14,19 @@ export const Customer = {
 
 export const CustomerBusiness = {
   async roles ({ id, customerId, sbi }, __, { dataSources }) {
+    if (!sbi) {
+      return null
+    }
+
     const authorisation = await dataSources.ruralPaymentsPortalApi.getAuthorisationByOrganisationId(sbi)
     return transformPersonRolesToCustomerAuthorisedBusinessesRoles(customerId, authorisation.personRoles)
   },
 
   async messages ({ id, customerId, sbi }, { pagination, showOnlyDeleted }, { dataSources }) {
+    if (!sbi) {
+      return null
+    }
+
     const notifications = await dataSources.ruralPaymentsPortalApi.getNotificationsByOrganisationIdAndPersonId(
       sbi,
       customerId,
@@ -30,12 +38,20 @@ export const CustomerBusiness = {
   },
 
   async permissionGroups ({ id, customerId, sbi }, __, { dataSources }) {
+    if (!sbi) {
+      return null
+    }
+
     return dataSources.permissions.getPermissionGroups().map(permissionGroup => ({ ...permissionGroup, businessId: sbi, customerId }))
   }
 }
 
 export const CustomerBusinessPermissionGroup = {
   async level ({ businessId, customerId, permissions, sbi }, __, { dataSources }) {
+    if (!sbi) {
+      return null
+    }
+
     const authorisation = await dataSources.ruralPaymentsPortalApi.getAuthorisationByOrganisationIdAndPersonId(sbi, customerId)
     return transformOrganisationAuthorisationToCustomerBusinessPermissionLevel(permissions, authorisation)
   }
