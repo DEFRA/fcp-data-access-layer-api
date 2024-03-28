5da97d5e44e962e4891887c239fffe906ab8c5da
Hadi <hadi.tajallaei@defra.gov.uk>
2024-03-28 08:44:22 +0000

    cosmetics

--- app/graphql/resolvers/business/query.js
+++ app/graphql/resolvers/business/query.js
@@ -9,6 +9,7 @@ export const Query = {
   async business (__, { id }, { dataSources }) {
     const response = await dataSources.ruralPaymentsPortalApi.getOrganisationBySBI(id)
     const business = transformOrganisationToBusiness(response)
+
     return {
       id,
       land: { sbi: id },
