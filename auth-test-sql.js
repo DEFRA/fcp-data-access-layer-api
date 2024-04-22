d92f5037fbe5086fb474caa754e5c305ea109cfa
defra-dom <164059781+defra-dom@users.noreply.github.com>
2024-04-22 16:38:42 +0100

    disable encryption

--- auth-test-sql.js
+++ auth-test-sql.js
@@ -5,11 +5,16 @@ const serverPassword = process.env.AUTHENTICATE_DB_PASSWORD
 const serverHost = process.env.AUTHENTICATE_DB_HOST
 const databaseName = process.env.AUTHENTICATE_DB_TABLE
 
-console.log(databaseName, serverUsername, serverPassword)
+console.log(serverHost, databaseName, serverUsername, serverPassword)
 
 const sequelizeMSSQL = new Sequelize(databaseName, serverUsername, serverPassword, {
   host: serverHost,
-  dialect: 'mssql'
+  dialect: 'mssql',
+  dialectOptions: {
+    options: {
+      encrypt: false,
+    },
+  },
 })
 
 const [results] = await sequelizeMSSQL.query("SELECT * FROM INFORMATION_SCHEMA.TABLES;");
