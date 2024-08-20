9af13fabe6dbdd117289b8b68192be01988bf48f
Dominic Taylor <dom.taylor@defra.gov.uk>
2024-08-20 15:07:50 +0100

    add insight transport for winston

--- app/utils/logger.js
+++ app/utils/logger.js
@@ -1,4 +1,5 @@
 import winston from 'winston'
+import { AzureApplicationInsightsLogger } from 'winston-azure-application-insights'
 
 Error.stackTraceLimit = Error.stackTraceLimit < 20 ? 20 : Error.stackTraceLimit
 
@@ -43,7 +44,9 @@ function jsonStringifyRecursive (obj) {
   )
 }
 
-const format = winston.format.combine(
+const level = process.env.LOG_LEVEL || 'info'
+const transports = []
+let format = winston.format.combine(
   winston.format.align(),
   winston.format.colorize(),
   winston.format.printf(info => {
@@ -66,8 +69,21 @@ const format = winston.format.combine(
   })
 )
 
-export const logger = winston.createLogger({
-  level: process.env.LOG_LEVEL || 'info',
-  transports: [new winston.transports.Console()],
+if (process.env.APPINSIGHTS_CONNECTIONSTRING) {
+  format = format.json()
+  transports.push(
+    new AzureApplicationInsightsLogger({
+      key: process.env.APPINSIGHTS_CONNECTIONSTRING
+    })
+  )
+} else {
+  transports.push(new winston.transports.Console())
+}
+
+const log = winston.createLogger({
+  level,
+  transports,
   format
 })
+
+export default log
