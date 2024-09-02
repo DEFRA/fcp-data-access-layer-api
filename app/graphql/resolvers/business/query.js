f925a77b0eaa444d71fdcb97c22bab49d0257809
Chris Salt-Mountain <37069825+csaltmountain@users.noreply.github.com>
2024-09-02 16:27:45 +0100

    handle business not being found

--- app/graphql/resolvers/business/query.js
+++ app/graphql/resolvers/business/query.js
@@ -1,9 +1,18 @@
+import { GraphQLError } from 'graphql'
 import { transformOrganisationToBusiness } from '../../../transformers/rural-payments/business.js'
 
 export const Query = {
   async business (__, { sbi }, { dataSources }) {
-    const response =
-      await dataSources.ruralPaymentsBusiness.getOrganisationBySBI(sbi)
+    const response = await dataSources.ruralPaymentsBusiness.getOrganisationBySBI(sbi)
+
+    if (!response) {
+      throw new GraphQLError('Business not found', {
+        extensions: {
+          code: 'NOT_FOUND'
+        }
+      })
+    }
+
     const business = transformOrganisationToBusiness(response)
     return {
       sbi,
