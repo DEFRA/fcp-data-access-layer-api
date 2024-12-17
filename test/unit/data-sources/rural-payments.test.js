6cd79b862d068135108553d993b1d0b8a1dcf5dd
Robin <robin.knipe@defra.gov.uk>
2024-12-17 17:12:14 +0000

    Fix: improve status code response (#117)
    * chore: format project settings file
    
    * chore: check for open handles after testing
    
    ...and ensure we're only testing changes made since the `dev` branch (not `main`).
    
    * fix: 502 status when RPP is unreachable
    
    Ensure the HTTP response code (502) correctly reflects that the Rural
    Payments service is unavailable.
    
    * chore: version bump v1.4.10
    
    * fix: improvements from PR
    
    Add detail to the error log, and refactor a test.

--- test/unit/data-sources/rural-payments.test.js
+++ test/unit/data-sources/rural-payments.test.js
@@ -1,4 +1,5 @@
-import { jest } from '@jest/globals'
+import { RESTDataSource } from '@apollo/datasource-rest'
+import { afterAll, beforeEach, describe, expect, jest, test } from '@jest/globals'
 import { RuralPayments } from '../../../app/data-sources/rural-payments/RuralPayments.js'
 import { APIM_APIM_REQUEST_001, RURALPAYMENTS_API_REQUEST_001 } from '../../../app/logger/codes.js'
 
@@ -8,59 +9,108 @@ const logger = {
 }
 
 describe('RuralPayments', () => {
-  test('handles RPP errors', () => {
-    // eslint-disable-next-line no-new
-    const rp = new RuralPayments({ logger })
+  describe('fetch', () => {
+    const mockFetch = jest.spyOn(RESTDataSource.prototype, 'fetch')
+    const dummyRequest = { method: 'POST' }
 
-    const error = new Error('test error')
-    error.extensions = { response: { status: 400, headers: { get: () => 'text/html' } } }
-    const request = {}
-    const url = 'test url'
+    beforeEach(() => {
+      mockFetch.mockReset()
+    })
+    afterAll(() => {
+      mockFetch.mockRestore()
+    })
 
-    rp.didEncounterError(error, request, url)
+    test('returns data from RPP', async () => {
+      const rp = new RuralPayments({ logger })
 
-    expect(logger.error).toHaveBeenCalledWith('#datasource - Rural payments - request error', {
-      error,
-      request,
-      response: error.extensions.response,
-      code: RURALPAYMENTS_API_REQUEST_001
+      mockFetch.mockResolvedValueOnce('data')
+
+      expect(await rp.fetch('path', dummyRequest)).toBe('data')
+      expect(mockFetch).toBeCalledTimes(1)
+    })
+
+    describe('throws upstream errors from RPP', () => {
+      test('when the RPP service encounters an error', async () => {
+        const error = new Error('Server error')
+        error.extensions = { response: { status: 500 } }
+
+        mockFetch.mockRejectedValue(error)
+
+        const rp = new RuralPayments({ logger })
+        try {
+          await rp.fetch('path', dummyRequest)
+        } catch (thrownError) {
+          expect(thrownError.extensions).toMatchObject({ response: { status: 500 } })
+          expect(mockFetch).toBeCalledTimes(3) // same as `maximumRetries` in RuralPayments.js
+        }
+        expect.assertions(2)
+      })
+
+      test('when the RPP service is totally unreachable', async () => {
+        mockFetch.mockRejectedValueOnce(new Error('ECONNREFUSED'))
+
+        const rp = new RuralPayments({ logger })
+        await expect(rp.fetch('path', dummyRequest)).rejects.toThrow(new Error('Bad Gateway'))
+        expect(mockFetch).toBeCalledTimes(1)
+      })
     })
   })
 
-  test('handles APIM errors with error.extensions', () => {
-    // eslint-disable-next-line no-new
-    const rp = new RuralPayments({ logger })
+  describe('didEncounterError', () => {
+    test('handles RPP errors', () => {
+      // eslint-disable-next-line no-new
+      const rp = new RuralPayments({ logger })
 
-    const error = new Error('test error')
-    error.extensions = { response: { status: 400 } }
-    const request = {}
-    const url = 'test url'
+      const error = new Error('test error')
+      error.extensions = { response: { status: 400, headers: { get: () => 'text/html' } } }
+      const request = {}
+      const url = 'test url'
 
-    rp.didEncounterError(error, request, url)
+      rp.didEncounterError(error, request, url)
 
-    expect(logger.error).toHaveBeenCalledWith('#datasource - apim - request error', {
-      error,
-      request,
-      response: error.extensions.response,
-      code: APIM_APIM_REQUEST_001
+      expect(logger.error).toHaveBeenCalledWith('#datasource - Rural payments - request error', {
+        error,
+        request,
+        response: error.extensions.response,
+        code: RURALPAYMENTS_API_REQUEST_001
+      })
     })
-  })
 
-  test('handles APIM errors without error.extensions', () => {
-    // eslint-disable-next-line no-new
-    const rp = new RuralPayments({ logger })
+    test('handles APIM errors with error.extensions', () => {
+      // eslint-disable-next-line no-new
+      const rp = new RuralPayments({ logger })
 
-    const error = new Error('test error')
-    const request = {}
-    const url = 'test url'
+      const error = new Error('test error')
+      error.extensions = { response: { status: 400 } }
+      const request = {}
+      const url = 'test url'
 
-    rp.didEncounterError(error, request, url)
+      rp.didEncounterError(error, request, url)
 
-    expect(logger.error).toHaveBeenCalledWith('#datasource - apim - request error', {
-      error,
-      request,
-      response: error?.extensions?.response,
-      code: APIM_APIM_REQUEST_001
+      expect(logger.error).toHaveBeenCalledWith('#datasource - apim - request error', {
+        error,
+        request,
+        response: error.extensions.response,
+        code: APIM_APIM_REQUEST_001
+      })
+    })
+
+    test('handles APIM errors without error.extensions', () => {
+      // eslint-disable-next-line no-new
+      const rp = new RuralPayments({ logger })
+
+      const error = new Error('test error')
+      const request = {}
+      const url = 'test url'
+
+      rp.didEncounterError(error, request, url)
+
+      expect(logger.error).toHaveBeenCalledWith('#datasource - apim - request error', {
+        error,
+        request,
+        response: error?.extensions?.response,
+        code: APIM_APIM_REQUEST_001
+      })
     })
   })
 })
